1b870ec464dec297fe7bd897c3a983967f6990c8
strngluv <strngluv@example.com>
2017-04-21 17:58:30 +0800

    Functional test now checks we can input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -2,6 +2,7 @@
 
 
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 
@@ -20,18 +21,33 @@ class NewVisitorTest(unittest.TestCase):
 
         #She noticed that both title and header of the page contain the word "TO-DO" 
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish the test!')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-DO', header_text)
 
         #The app invite her to enter a to-do item
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get-attribute('placeholder'),
+            'Enter a to-do item'
+        )
 
         #She enters "Buy peacock feathers" into the text area
         #Her habbit is to use make fly
+        inputbox.send_keys('Buy peacock feathers')
 
         #After hitting ENTER, the page refreshed
         #The To-Do list shows "1: Buy peacock freathers"
+        inputbox.send_keys(keys.ENTER)
+        
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy peacock feathers' for row in rows)
+        )
 
         #The page shows another text area to enter other To-Do items
         #She enters "Use peacock feathers to make a fly" 
+        self.fail('Finish the test!')
 
         #The page refreshed again,there are two items in her list
 
