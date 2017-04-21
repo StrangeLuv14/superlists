4b749e4248fbcbca60dcabc1f5531c63c46d93bf
strngluv <strngluv@example.com>
2017-04-22 01:30:23 +0800

    add more functional tests

--- functional_tests/test.py
+++ functional_tests/test.py
@@ -21,7 +21,7 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertIn(row_text, [row.text for row in rows])
         
     def test_can_start_a_list_and_retrive_it_later(self):
-        #Idis heard there is a super cool online TO-DO app
+        #Idith heard there is a super cool online TO-DO app
         #She goes to check the app's page
         self.browser.get(self.live_server_url)
 
@@ -41,9 +41,11 @@ class NewVisitorTest(LiveServerTestCase):
         #Her habbit is to use make fly
         inputbox.send_keys('Buy peacock feathers')
 
-        #After hitting ENTER, the page refreshed
-        #The To-Do list shows "1: Buy peacock feathers"
+        #After hitting ENTER, she is brought to a new URL
+        #The To-Do list in this page shows "1: Buy peacock feathers"
         inputbox.send_keys(Keys.ENTER)
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url, '/lists/.+')
         self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         #The page shows another text area to enter other To-Do items
@@ -56,11 +58,38 @@ class NewVisitorTest(LiveServerTestCase):
         self.check_for_row_in_list_table('1: Buy peacock feathers')
         self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
-        #Idis wants to know wether this website could remember her list
+        #Idith wants to know wether this website could remember her list
         #She saw the website generate a unique URL for her
         #And there are some text to explain this feature
         self.fail('Finish the test!')
 
-        #She checked that URL,found that her list is still there
-
-        #She is satisfied,and gone to sleep
+        #Now a new user called Francis visit the website
+        
+        ##We use a new browser dialog
+        ##Make sure Idith's info won't leak from cookie
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
+        
+        #Francis visits home page
+        #The home page won't show Idith's To-Do list
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertNotIn('make a fly', page_text)
+        
+        #Francis enter a new item, make a new To-Do list
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy milk')
+        inputbox.send_keys(Keys.ENTER)
+        
+        #Francis get a unique URL of his own
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url, '/lists/.+')
+        self.assertNotEqual(francis_list_url, edith_list_url)
+        
+        #This page still do not have edith's list
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertIn('Buy milk', page_text)
+        
+        #Both of them are satisfied, and gone to sleep
