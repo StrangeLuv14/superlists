9af7a6cd796f1a403bbad602392547d1894c5a41
strngluv <strngluv@example.com>
2017-04-21 21:47:28 +0800

    Refactor functional_tests.py

--- functional_tests.py
+++ functional_tests.py
@@ -14,6 +14,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
         
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_tag_name('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+        
     def test_can_start_a_list_and_retrive_it_later(self):
         #Idis heard there is a super cool online TO-DO app
         #She goes to check the app's page
@@ -50,10 +55,8 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys(Keys.ENTER)
 
         #The page refreshed again,there are two items in her list
-        tabel = self.browser.find_element_by_id('id_list_table')
-        rows = tabel.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-        self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make fly')
 
         #Idis wants to know wether this website could remember her list
         #She saw the website generate a unique URL for her
