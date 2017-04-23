711e73af40b7a510a84bc9f625b6d4c0f8ddba78
strngluv <strngluv@example.com>
2017-04-23 15:14:46 +0800

    move sqlite database outside of main source tree

--- functional_tests/test.py
+++ functional_tests/test.py
@@ -3,11 +3,16 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 import time
+import os
 
 
 class NewVisitorTest(StaticLiveServerTestCase):
+    
     def setUp(self):
         self.browser = webdriver.Firefox()
+        staging_server = os.environ.get('STAGING_SERVER')
+        if staging_server:
+            self.live_server_url = 'http://' + staging_server
         self.browser.implicitly_wait(3)
         
     def tearDown(self):
