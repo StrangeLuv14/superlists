487d78e871352becb4c4eac4e235a5b1de8a6b9a
strngluv <strngluv@example.com>
2017-04-25 01:09:40 +0800

    Split out unit tests into two files,fixed bug:lists/urls.py(call url with POST,the urlconf should not have slash in the end.)

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -20,38 +20,9 @@ class HomePageTest(TestCase):
         
         response = home_page(request)
         
-        self.assertTrue(response.content.startswith(b'<html>'))
+        self.assertTrue(response.content.startswith(b'<!DOCTYPE html>'))
         self.assertIn(b'<title>To-Do lists</title>', response.content)
         self.assertTrue(response.content.endswith(b'</html>'))
-        
-        
-class ListAndItemModelTest(TestCase):
-    def test_saving_and_retrieving_items(self):
-        list_ = List()
-        list_.save()
-        
-        first_item = Item()
-        first_item.text = 'The first (ever) list item'
-        first_item.list = list_
-        first_item.save()
-        
-        second_item = Item()
-        second_item.text = 'Item the second'
-        second_item.list = list_
-        second_item.save()
-        
-        saved_list = List.objects.first()
-        self.assertEqual(saved_list, list_)
-        
-        saved_items = Item.objects.all()
-        self.assertEqual(saved_items.count(), 2)
-        
-        first_saved_item = saved_items[0]
-        second_saved_item = saved_items[1]
-        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
-        self.assertEqual(first_saved_item.list, list_)
-        self.assertEqual(second_saved_item.text, 'Item the second')
-        self.assertEqual(second_saved_item.list, list_)
         
         
 class ListViewTest(TestCase):
