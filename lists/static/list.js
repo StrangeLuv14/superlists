8638550424d8f020e85e4ae8ec8b39c41f8682ef
strngluv <strngluv@example.com>
2017-04-29 17:51:12 +0800

    add jquery, qunit tests, list.js with keypress listeners

---
+++ lists/static/list.js
@@ -0,0 +1,8 @@
+var initialize = function () {
+	console.log('initialize called');
+	$('input[name="text"]').on('keypress', function () {
+		console.log('in keyress handler');
+		$('.has-error').hide();
+	});
+};
+console.log('list.js loaded');
